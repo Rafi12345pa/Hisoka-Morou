de31a37a89bb3f2b8bad2764b86507e742be9c45
MuhammadRafi <108687316+Rafi12345pa@users.noreply.github.com>
2022-07-30 17:00:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.APIKeys = {
 // Other
 global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
 global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.packname = 'Hai Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
